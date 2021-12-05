41a4c1927d5bce6be3143b9d9ca1c74d13850db0
Asmaa <Asmaa99musse@gmail.com>
2021-12-05 21:29:15 +0000

    added middleware paths to server.js

--- src/server.js
+++ src/server.js
@@ -2,11 +2,23 @@ const fs = require("fs"),
 const path = require("path"),
 
 const express = require("express"),
-const notes = require("./db/db.json"),
-const uuid = require("uuid"),
+const routes = require("./routes");
+
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+
+// Middleware
+
+app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.urlencoded({ extended:true }));
+app.use(express.json({ extended:true }));
+app.use(routes);
+
+app.listen(PORT, () => 
+  console.log(`Running on https://loalhost:${PORT}`));
+
 
 
 
