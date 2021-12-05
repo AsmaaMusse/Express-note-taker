20c65e85cd2cb44714d5a63d9366f350ecb13b02
Asmaa <Asmaa99musse@gmail.com>
2021-12-05 21:44:30 +0000

    created syntax for read and write to a file

---
+++ src/util.js
@@ -0,0 +1,22 @@
+const fs = require("fs"),
+const path = require("path"),
+
+const writeToFile = (fileName, data) => {
+    fs.writeFileSync(
+        path.join(__dirname, "../db/db.json.json"), 
+        JSON.stringify(data)
+    );
+};
+
+const readFromFile = (fileName) => {
+    const jsonData = fs.readFileSync(
+        path.join(__dirname, "../db/db.json"),"utf8"
+    );
+
+    return JSON.parse(jsonData);
+};
+
+module.exports = {
+    writeToFile,
+    readFromFile,
+};
