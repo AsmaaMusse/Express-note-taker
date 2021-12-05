38fa33e52f6dce152fae1c4e42d081857964c717
Asmaa <Asmaa99musse@gmail.com>
2021-12-05 20:56:31 +0000

    imported required files to server.js

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const fs = require("fs"),
+const path = require("path"),
+
+const express = require("express"),
+const notes = require("./db/db.json"),
+const uuid = require("uuid"),
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+
+
+
+
+
+
+
+
+
