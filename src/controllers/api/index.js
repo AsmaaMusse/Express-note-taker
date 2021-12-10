1e7ba52e0fda6233489f0e9aa15e5ca5c617c076
Asmaa <Asmaa99musse@gmail.com>
2021-12-10 20:52:43 +0000

    created get,post and delete notes functions

--- src/controllers/api/index.js
+++ src/controllers/api/index.js
@@ -1,34 +1,43 @@
+const { v4: uuidv4 } = require("uuid");
 const { readFromFile, writeToFile } = require("../../util");
 // const  = require("../../util");
 
 const getAppNotes = (req, res) => {
-  // readFromFile and get notes
-  const notes = readFromFile();
-  // parse the notes data
-  console.log(notes);
-  // send the json response
-  res.json(notes);
+  try {
+    const notes = readFromFile();
+    console.log(notes);
+    res.json(notes);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "Error!:getAppNotes" });
+  }
 };
 
 const postAppNotes = (req, res) => {
-  // get the data from req body
+  try {
+    const note = readFromFile();
+    const { title, text } = req.body;
+    const id = uuidv4();
 
-  // console.log(req.body);
-  console.log(req.body.title);
-  // read notes from file
-  // parse notes
-  // push new note to array
-  // write to file
-  const notes = readFromFile();
-  // send response
+    const newNote = { title, text, id };
+    writeToFile("db, [newNote, ...noteData]");
+    res.json(newNote);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "Error!:postAppNotes" });
+  }
 };
 
 const deleteAppNotes = (req, res) => {
-  // get the id of the note to delete
-  // read all notes from file
-  // filter the notes and remove the note
-  // write the notes to file
-  // send response
+  try {
+    const { id } = req.params;
+    const deleteNote = readFromFile().filter((note) => note.id !== id);
+    writeToFile("db", deleteNote);
+    res.status(200).json({ message: "your note has been delete" });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "Error!:deleteAppNotes" });
+  }
 };
 
 module.exports = {
